2e9a11fd1e8c441daa943ec9227d68064894f64d
Gray Louis Putnam <gputnam@lxplus089.cern.ch>
2016-06-30 09:40:49 +0200

    merge runcp

--- run_cp.sh
+++ run_cp.sh
@@ -1,10 +1,15 @@
-re=trks-trks-j0pT-trks
 inch=F_c10-cb
-ch=f_c10-cb$"_"$re
-#ch=f_c10-cb
-homepath="/afs/cern.ch/user/b/btong/"
-workpath="/afs/cern.ch/work/b/btong/bbbb/CHEPAnalysis/Output/"
-python PlotTinyTree.py --inputdir $inch --outputdir $ch --reweight "test" #$re
+# inch=b77_c10-cb
+#ch=b77_c10-cb$"_"$re
+ch=f_c10-cb-$1-$2
+homepath="/afs/cern.ch/user/g/gputnam/"
+workpath="/afs/cern.ch/work/g/gputnam/public/Output/"
+reweight="run$1"
+
+echo $reweight
+echo $ch
+
+python PlotTinyTree.py --inputdir $inch --outputdir $ch --reweight $reweight #$re
 python get_count.py --inputdir $ch
 python plot.py --inputdir $ch
 python reweight.py --inputdir $ch
@@ -21,4 +26,5 @@ find $workpath$ch$"/Plot/Sideband/" -name '*.png' -exec cp {} $homepath"/www/sha
 find $workpath$ch$"/Plot/Control/" -name '*.png' -exec cp {} $homepath"/www/share/hh4b/plot/"$ch \;
 cd $homepath"/www/share/"
 python createHtmlOverview.py
+cd ~/public/Xhh45/MakePlot
 echo "Done!"
