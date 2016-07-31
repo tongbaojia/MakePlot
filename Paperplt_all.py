1f0380884625c49da91145d48ca06d2d417a4705
Tony Tong <baojia.tong@cern.ch>
2016-07-31 10:39:38 +0200

    add in predicted 2D distributions

--- Paperplt_all.py
+++ Paperplt_all.py
@@ -36,6 +36,7 @@ def main():
 
     # # paper plot
     DrawPaper2D("data_test/hist-MiniNTuple.root", "NoTag_Incl", prename="NoTag_Incl", Xrange=[50, 250], Yrange=[50, 250])
+    DrawPaper2DPrediction("data_test/hist-MiniNTuple.root", "NoTag_Incl", prename="TwoTag_Incl", Xrange=[50, 250], Yrange=[50, 250])
 
 def DrawPaper2D(inputname, inputdir, keyword="_", prename="", Xrange=[0, 0], Yrange=[0, 0]):
     # functions for the different regions
@@ -157,6 +158,146 @@ def DrawPaper2D(inputname, inputdir, keyword="_", prename="", Xrange=[0, 0], Yra
     canv.Close()
     inputroot.Close()
 
+def DrawPaper2DPrediction(inputname, inputdir, keyword="_", prename="", Xrange=[0, 0], Yrange=[0, 0]):
+    # functions for the different regions
+    def myCR(x):
+        return  ROOT.TMath.Sqrt( (x[0]-124)**2 + (x[1]-115)**2)
+
+    def mySR(x):
+        return  ROOT.TMath.Sqrt( ((x[0]-124)/(0.1*x[0]))**2 + ((x[1]-115)/(0.1*x[1]))**2)
+
+    def myTop(x):
+        return  ROOT.TMath.Sqrt( ((x[0]-175)/(0.1*x[0]))**2 + ((x[1]-164)/(0.1*x[1]))**2 )  
+
+
+    #print inputdir
+    inputroot = ROOT.TFile.Open(inputpath + inputname)
+    inputroot_top = ROOT.TFile.Open(inputpath + "ttbar_comb_test/hist-MiniNTuple.root")
+    #inputroot.cd(inputdir)
+    temp_hist = inputroot.Get("NoTag_2Trk_split_Incl" + "/mH0H1").Clone()
+    temp_hist_top = inputroot_top.Get("NoTag_2Trk_split_Incl" + "/mH0H1").Clone()
+
+    #scale and add
+    inputtex = CONF.inputpath + "b77/Plot/Tables/normfit.tex"
+    f1 = open(inputtex, 'r')
+    for line in f1: 
+        #very stupid protection to distinguish 2b and 2bs
+        if ("Nb=2s") in line:
+            templine = line.split("&")
+            tempqcd = templine[1].split(" ")
+            muqcd = float(tempqcd[1])
+            muqcd_err = float(tempqcd[3])
+            temptop = templine[2].split(" ")
+            mutop = float(temptop[1])
+            mutop_err = float(temptop[3])
+            #print " muqcd ", muqcd, " atop ", mutop
+            temp_hist.Scale(muqcd)
+            temp_hist_top.Scale(mutop)
+            temp_hist.Add(temp_hist_top)
+    
+    #proceed
+    canv = ROOT.TCanvas(temp_hist.GetName(), temp_hist.GetTitle(), 1000, 800)
+    if Xrange != [0, 0]:
+        temp_hist.GetXaxis().SetRangeUser(Xrange[0], Xrange[1])
+    if Yrange != [0, 0]:
+        temp_hist.GetYaxis().SetRangeUser(Yrange[0], Yrange[1])
+        temp_hist.GetYaxis().SetTitleOffset(1.5)
+    #print Xrange, Yrange, temp_hist.GetXaxis().GetMinimum(), temp_hist.GetXaxis().GetMaximum()
+    canv.SetLeftMargin(0.17)
+    canv.SetRightMargin(0.23)
+    # log scale
+    #canv.SetLogz(1)
+    temp_hist.Draw("colz")
+    # Set Axis Labels
+    temp_hist.GetXaxis().SetTitle("m_{J}^{lead} [GeV]")
+    temp_hist.GetYaxis().SetTitle("m_{J}^{subl} [GeV]")
+    temp_hist.GetZaxis().SetTitle("Events/10 GeV^{2}")
+    temp_hist.GetZaxis().SetTitleOffset(1.8)
+    temp_hist.GetZaxis().SetRangeUser(0, temp_hist.GetMaximum())
+    # change divisions
+    temp_hist.GetXaxis().SetNdivisions(505)
+    temp_hist.GetYaxis().SetNdivisions(505)
+    temp_hist.GetZaxis().SetNdivisions(505)
+
+    # Draw Signal Region
+    thetas = np.linspace(-np.pi, np.pi, 50)
+
+    # get signal points:
+    fSR = ROOT.TF2("SR",mySR,0.,Xrange[1],0.,Xrange[1])
+    contorsSR = array.array("d", [1.6])
+    fSR.SetContour(len(contorsSR),contorsSR)
+    fSR.SetNpx(50)
+    fSR.SetLineWidth(6)
+    fSR.SetLineStyle(5)
+    fSR.Draw("same, cont3")
+
+    # get control:
+    fCR = ROOT.TF2("CR", myCR,0,Xrange[1],0,Xrange[1])
+    contoursCR = array.array("d", [35.8])
+    fCR.SetContour(1, contoursCR)
+    fCR.SetNpx(200)
+    fCR.SetLineWidth(6)
+    fCR.Draw("same, cont3")
+
+    # sideband:
+    fSB = ROOT.TF2("SB", myCR,0,Xrange[1],0,Xrange[1])
+    contoursSB = array.array("d", [63.0])
+    fSB.SetContour(1, contoursSB)
+    fSB.SetNpx(200)
+    fSB.SetLineWidth(6)
+    fSB.Draw("same, cont3")
+
+    # ttbar:
+    fTT = ROOT.TF2("TT", myTop,0,Xrange[1],0,Xrange[1])
+    contoursTT = array.array("d", [1.0])
+    fTT.SetContour(1, contoursTT)
+    fTT.SetNpx(50)
+    fTT.SetLineColor(46)
+    fTT.SetLineWidth(3)
+    fTT.SetLineStyle(5)
+    #fTT.Draw("same, cont3")
+    # ttbar label:
+    ttb_txt = ROOT.TLatex(0.65, 0.75, "#splitline{t#bar{t} enriched}{region}")
+    ttb_txt.SetTextColor(46)
+    ttb_txt.SetTextSize(0.03)
+    #helpers.DrawWords(ttb_txt)
+
+    # fill box
+    fillbox = ROOT.TBox(Xrange[0], Yrange[1]-21, Xrange[1], Yrange[1])
+    fillbox.SetFillStyle(1001)
+    fillbox.SetFillColor(0)
+    # line box
+    linebox = ROOT.TBox(Xrange[0], Yrange[1]-21, Xrange[1], Yrange[1])
+    linebox.SetFillStyle(0)
+    linebox.SetLineWidth(2)
+    linebox.SetLineStyle(1)
+    linebox.SetLineColor(ROOT.kBlack)
+
+    linebox.Draw("same")
+    fillbox.Draw("same")
+
+    # Draw Watermarks
+    xatlas, yatlas = 0.3, 0.9
+    atlas = ROOT.TLatex(xatlas, yatlas, "ATLAS Internal")
+    hh4b  = ROOT.TLatex(xatlas+0.3, yatlas, "#sqrt{s}=13 TeV, " + str(CONF.totlumi) + " fb^{-1}")
+    watermarks = [atlas, hh4b]
+    for wm in watermarks:
+        wm.SetTextAlign(24)
+        wm.SetTextSize(0.035)
+        wm.SetTextFont(42)
+        wm.SetNDC()
+        wm.Draw()
+
+    canv.SaveAs(outputpath + prename + "_" +  canv.GetName() + ".pdf")
+    canv.SaveAs(outputpath + prename + "_" +  canv.GetName() + ".png")
+    canv.SaveAs(outputpath + prename + "_" +  canv.GetName() + ".eps")
+    canv.SaveAs(outputpath + prename + "_" +  canv.GetName() + ".C")
+
+    #shut it down
+    canv.Close()
+    inputroot.Close()
+    inputroot_top.Close()
+    f1.close()
 
 if __name__ == "__main__":
     main()
